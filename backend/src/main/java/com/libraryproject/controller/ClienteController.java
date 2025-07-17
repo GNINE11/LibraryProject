dd0d215e5c544fc0b004585c359a617ad3ea8aaf
GNINE11 <gabriel.souza16@estudante.ufla.br>
2025-07-17 19:03:38 -0300

    fix(controller): corrige endpoint DELETE para cliente

--- backend/src/main/java/com/libraryproject/controller/ClienteController.java
+++ backend/src/main/java/com/libraryproject/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,13 +45,14 @@ public class ClienteController {
         return ResponseEntity.ok(clientes);
     }
 
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
         if (clienteService.buscarPorId(id).isPresent()){
             clienteService.deletarPorId(id);
             return ResponseEntity.noContent().build();
-        }
-        else{
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
+
 }
