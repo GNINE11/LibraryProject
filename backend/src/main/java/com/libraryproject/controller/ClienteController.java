27fc5cf4cfe2457850d38476982ca329de57714a
GNINE11 <gabriel.souza16@estudante.ufla.br>
2025-07-17 18:50:00 -0300

    feat(controller): início da criação dos controllers

---
+++ backend/src/main/java/com/libraryproject/controller/ClienteController.java
@@ -0,0 +1,56 @@
+package com.libraryproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libraryproject.model.Cliente;
+import com.libraryproject.service.ClienteService;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente){
+        Cliente salvo = clienteService.salvar(cliente);
+        return ResponseEntity.status(201).body(salvo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+        return clienteService.buscarPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/por-email")
+    public ResponseEntity<Cliente> buscarPorEmail(@RequestParam String email){
+        return clienteService.buscarPorEmail(email).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarTodos(){
+        List<Cliente> clientes = clienteService.listarTodos();
+        return ResponseEntity.ok(clientes);
+    }
+
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        if (clienteService.buscarPorId(id).isPresent()){
+            clienteService.deletarPorId(id);
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
