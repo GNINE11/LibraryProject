468ca5120e7125ba4cc41596666946b6e3ebcd3c
GNINE11 <gabriel.souza16@estudante.ufla.br>
2025-07-20 15:40:59 -0300

    feat: adiciona controller da entidade ItemPedido

---
+++ backend/src/main/java/com/libraryproject/controller/ItemPedidoController.java
@@ -0,0 +1,61 @@
+package com.libraryproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libraryproject.model.ItemPedido;
+import com.libraryproject.model.Pedido;
+import com.libraryproject.service.ItemPedidoService;
+
+@RestController
+@RequestMapping("/itens-pedido")
+public class ItemPedidoController {
+    
+    @Autowired
+    private ItemPedidoService itemPedidoService;
+
+    @PostMapping
+    public ResponseEntity<ItemPedido> salvar(@RequestBody ItemPedido itemPedido){
+        ItemPedido salvo = itemPedidoService.salvar(itemPedido);
+        return ResponseEntity.status(201).body(salvo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemPedido> buscarPorId(@PathVariable Long id){
+        return itemPedidoService.buscarPorId(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/pedido/{pedidoId}")
+    public ResponseEntity<List<ItemPedido>> buscarPorPedido(@PathVariable Long pedidoId){
+        Pedido pedido = new Pedido();
+        pedido.setId(pedidoId);
+        List<ItemPedido> itensPedido = itemPedidoService.buscarPorPedido(pedido);
+        return ResponseEntity.ok(itensPedido);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemPedido>> listarTodos(){
+        List<ItemPedido> itensPedido = itemPedidoService.listarTodos();
+        return ResponseEntity.ok(itensPedido);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable Long id){
+        if (itemPedidoService.buscarPorId(id).isPresent()){
+            itemPedidoService.deletarPorId(id);
+            return ResponseEntity.noContent().build();
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
--- backend/src/main/java/com/libraryproject/model/ItemPedido.java
+++ backend/src/main/java/com/libraryproject/model/ItemPedido.java
@@ -17,7 +17,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "ItemPedido")
+@Table(name = "itempedido")
 public class ItemPedido {
     
     @Id
