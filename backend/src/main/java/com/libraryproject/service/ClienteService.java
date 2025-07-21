394c21d1f8751ed7e6b52f960b59f04975b42aca
GNINE11 <gabriel.souza16@estudante.ufla.br>
2025-07-20 23:15:52 -0300

    feat: atualiza endpoints PUT/PATCH de cliente

--- backend/src/main/java/com/libraryproject/service/ClienteService.java
+++ backend/src/main/java/com/libraryproject/service/ClienteService.java
@@ -31,6 +31,41 @@ public class ClienteService {
         return clienteRepository.findAll();
     }
 
+    public Cliente atualizar(Long id, Cliente novoCliente){
+        return clienteRepository.findById(id)
+            .map(clienteExistente -> {
+                clienteExistente.setNome(novoCliente.getNome());
+                clienteExistente.setTelefone(novoCliente.getTelefone());
+
+                return clienteRepository.save(clienteExistente);
+            }).orElseThrow(() -> new RuntimeException("Cliente não encontrado com id: " + id));
+    }
+
+    public Cliente atualizarEmail(Long id, String novoEmail) {
+        Cliente cliente = clienteRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        
+        if (clienteRepository.findByEmail(novoEmail).isPresent()) {
+            throw new RuntimeException("Email já está em uso");
+        }
+        
+        if (!cliente.getEmail().equals(novoEmail)) {
+            cliente.setEmail(novoEmail);
+            return clienteRepository.save(cliente);
+        }
+        
+        return cliente;
+    }
+
+    public void atualizarSenha(Long id, String novaSenha) {
+        Cliente cliente = clienteRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+        
+        //Criptografar dps
+        cliente.setSenha(novaSenha);
+        clienteRepository.save(cliente);
+    }
+
     public void deletarPorId(Long id){
         clienteRepository.deleteById(id);
     }
